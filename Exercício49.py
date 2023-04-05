30a26056e461b063918872d5a707f2bf2e0080d0
BorgesAbraao10 <abraao10clear@gmail.com>
2023-04-05 11:44:33 -0300

    tab

---
+++ Exercício49.py
@@ -0,0 +1,7 @@
+# Exercício Python 49: Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+num = int (input('Digite um número para saber a sua tabuada:'))
+for c in range (0,11):
+    print ('{} x {:2} = {}'.format(num,c,num*c))
+
+    
